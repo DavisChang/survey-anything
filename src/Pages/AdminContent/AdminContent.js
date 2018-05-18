ab7e5cd3780901e257991a98dce6d22b43275428
davis_chang <davis chang>
2018-05-18 10:54:43 +0800

    target page

--- src/Pages/AdminContent/AdminContent.js
+++ src/Pages/AdminContent/AdminContent.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import { FormControl, Checkbox, FormGroup, ControlLabel, HelpBlock, Button } from 'react-bootstrap';
 import './AdminContent.css';
+import { Link } from "react-router-dom";
+
+const FieldGroup = ({ id, label, help, ...props }) => {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+};
 
 class AdminContent extends Component {
 
@@ -8,8 +20,15 @@ class AdminContent extends Component {
     this.state = {
       step: '123',
       surveyName: '',
+      urlPath: '',
     };
   }
+
+  onChangeUrlPath = event => {
+    this.setState({
+      urlPath: event.target.value,
+    });
+  };
 
   onChangeSurveyName = event => {
     this.setState({
@@ -29,7 +48,62 @@ class AdminContent extends Component {
               <h1 className="title">Target</h1>
               <div className="target-survey-title">
                 <p>問卷名稱:</p>
-                <input type="text" value={this.state.surveyName} onChange={this.onChangeSurveyName}/>
+                <FormControl type="text" placeholder="VIVEPORT Subscription NPS" onChange={this.onChangeSurveyName} value={this.state.surveyName} />
+              </div>
+              <div className="target-survey-control">
+                <div className="target-survey-control-block">
+                  <p>目標群眾</p>
+                  <div>
+                    <Checkbox>Web</Checkbox>
+                    <Checkbox>AIO Device</Checkbox>
+                    <Checkbox>VIVE Device</Checkbox>
+                    <Checkbox>PC Client</Checkbox>
+                    <Checkbox>MAC Client</Checkbox>
+                    <Checkbox>In VR Content</Checkbox>
+                  </div>
+                </div>
+                <div className="target-survey-control-block">
+                  <p>條件：</p>
+                  <div>
+                    <FormGroup controlId="formControlsSelect">
+                      <ControlLabel>國家</ControlLabel>
+                      <FormControl componentClass="select" placeholder="select">
+                        <option value="TW">Taiwan</option>
+                        <option value="US">United States</option>
+                        <option value="CN">China</option>
+                        <option value="UK">United Kingdom</option>
+                      </FormControl>
+                    </FormGroup>
+                    <FormGroup controlId="formControlsSelect">
+                      <ControlLabel>性別</ControlLabel>
+                      <FormControl componentClass="select" placeholder="select">
+                        <option value="m">男性</option>
+                        <option value="w">女性</option>
+                      </FormControl>
+                    </FormGroup>
+                  </div>
+                </div>
+                <div className="target-survey-control-block">
+                  <p>其他設定：</p>
+                  <div>
+                    <FormGroup controlId="formControlsSelect">
+                      <ControlLabel>URL Path</ControlLabel>
+                      <FormControl type="text" placeholder="/subscriptions/:SubID" onChange={this.onChangeUrlPath} value={this.state.urlPath} />
+                    </FormGroup>
+                    <FieldGroup
+                      id="formControlsFile"
+                      type="file"
+                      label="客製化"
+                      help="Example block-level help text here."
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="target-survey-title">
+                <Button>
+                  <Link to="/admin">返回</Link>
+                </Button>
+                <Button bsStyle="success"><Link to="/admin/questions">下一步</Link></Button>
               </div>
             </div>
           </div>
